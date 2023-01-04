efd9fc9ad3327c4ced7a9b733c98f57f29ccfb0a
Cyrto <1461638083@qq.com>
2023-01-04 15:27:29 +0800

    feat: upgrade payment result types

--- types.go
+++ types.go
@@ -65,16 +65,30 @@ type (
 	PaymentsEstimatedResp = MixPayResp[PaymentsEstimated]
 
 	PaymentsResult struct {
-		Status           string `json:"status"`
-		QuoteAmount      string `json:"quoteAmount"`
-		QuoteSymbol      string `json:"quoteSymbol"`
-		PaymentAmount    string `json:"paymentAmount"`
-		PaymentSymbol    string `json:"paymentSymbol"`
-		Payee            string `json:"payee"`
-		PayeeMixinNumber string `json:"payeeMixinNumber"`
-		PayeeAvatarURL   string `json:"payeeAvatarUrl"`
-		Txid             string `json:"txid"`
-		Date             any    `json:"date"`
+		Status           string      `json:"status"`
+		QuoteAmount      string      `json:"quoteAmount"`
+		QuoteSymbol      string      `json:"quoteSymbol"`
+		QuoteAssetID     string      `json:"quoteAssetId"`
+		PaymentAmount    string      `json:"paymentAmount"`
+		PaymentSymbol    string      `json:"paymentSymbol"`
+		PaymentAssetID   string      `json:"paymentAssetId"`
+		Payee            string      `json:"payee"`
+		PayeeID          string      `json:"payeeId"`
+		PayeeMixinNumber string      `json:"payeeMixinNumber"`
+		PayeeAvatarURL   string      `json:"payeeAvatarUrl"`
+		Txid             string      `json:"txid"`
+		BlockExplorerURL string      `json:"blockExplorerUrl"`
+		Date             int64       `json:"date"`
+		SurplusAmount    string      `json:"surplusAmount"`
+		SurplusStatus    string      `json:"surplusStatus"`
+		Confirmations    int64       `json:"confirmations"`
+		PayableAmount    string      `json:"payableAmount"`
+		FailureCode      string      `json:"failureCode"`
+		FailureReason    string      `json:"failureReason"`
+		ReturnTo         string      `json:"returnTo"`
+		TraceID          string      `json:"traceId"`
+		Nonce            interface{} `json:"nonce"`
+		Remark           string      `json:"remark"`
 	}
 	PaymentsResultResp = MixPayResp[PaymentsResult]
 )
